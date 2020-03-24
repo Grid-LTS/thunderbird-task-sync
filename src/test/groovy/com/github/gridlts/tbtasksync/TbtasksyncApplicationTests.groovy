8f9a122f88427edf7b01cf0ff4e0ff7ac398d76b
Gerd Friemel <gerd.friemel@gmail.com>
2020-03-24 23:12:08 +0100

    [INIT] intial commit with sceleton Spring boot groovy project.

---
+++ src/test/groovy/com/github/gridlts/tbtasksync/TbtasksyncApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.gridlts.tbtasksync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TbtasksyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
