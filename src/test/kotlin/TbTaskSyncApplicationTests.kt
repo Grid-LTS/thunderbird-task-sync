7a677b5b5230a01b76b26d3883e4b9cdef844adf
Gerd Friemel <gerd.friemel@friemel-tech.de>
2025-03-30 21:42:36 +0200

    fix: migrate also tests to Kotlin

--- src/test/kotlin/TbTaskSyncApplicationTests.kt
+++ src/test/kotlin/TbTaskSyncApplicationTests.kt
@@ -1,19 +1,17 @@
 package com.github.gridlts.tbtasksync
 
-
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@Import(TestConfig.class)
+@Import(TestConfig::class)
 @ActiveProfiles("test")
 class TbTaskSyncApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
-
+    @Test
+    fun contextLoads() {
+        // Your test code here
+    }
 }
--- src/test/kotlin/TestConfig.kt
+++ src/test/kotlin/TestConfig.kt
@@ -2,31 +2,26 @@ package com.github.gridlts.tbtasksync
 
 import com.github.gridlts.tbtasksync.google.GTaskRepo
 import com.github.gridlts.tbtasksync.google.GoogleAuthorization
-import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
-
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when
+import org.mockito.Mockito.*
 
 @TestConfiguration
 class TestConfig {
 
     @MockBean
-    GTaskRepo gTaskRepo
+    lateinit var gTaskRepo: GTaskRepo
 
     @Bean
     @Primary
-    GoogleAuthorization mockGoogleAuthorization() {
-        GoogleAuthorization authorization = mock(GoogleAuthorization.class)
-        GoogleCredential mockCredential = new GoogleCredential()
-        mockCredential.setAccessToken("gud")
-        when(authorization.main()).thenReturn(mockCredential);
+    fun mockGoogleAuthorization(): GoogleAuthorization {
+        val authorization = mock(GoogleAuthorization::class.java)
+        val mockCredential = GoogleCredential()
+        mockCredential.accessToken = "gud"
+        `when`(authorization.main()).thenReturn(mockCredential)
         return authorization
     }
-
-
 }
