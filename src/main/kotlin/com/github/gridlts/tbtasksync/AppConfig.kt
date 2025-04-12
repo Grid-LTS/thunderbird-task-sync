375905db8ace8bbf6ea3614374d92aae74eaa1e8
Gerd Friemel <gerd.friemel@friemel-tech.de>
2025-04-12 17:51:58 +0200

    fix: add integration test database

--- src/main/kotlin/com/github/gridlts/tbtasksync/AppConfig.kt
+++ src/main/kotlin/com/github/gridlts/tbtasksync/AppConfig.kt
@@ -8,30 +8,42 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.env.Environment
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.ResourceLoader
 import java.nio.file.Path
-import javax.sql.DataSource
 import java.nio.file.Paths
+import javax.sql.DataSource
+
 
 @Configuration
 class AppConfig {
     @Value("\${thunderbird.profile}")
     private lateinit var thunderbirdProfile: String
 
+
+    @Autowired
+    private lateinit var resourceLoader: ResourceLoader
+
+
+    @Value("\${user.home}")
+    private lateinit var userHomeDir: String
+
     @Autowired
     private lateinit var env: Environment
 
+
     @Bean
     fun thunderbirdProfilePath(): Path {
-        return if (env.activeProfiles.contains("test")) {
-            Paths.get(System.getProperty("user.dir"),"tmp")
+        return if (env.activeProfiles.contains("test") || env.activeProfiles.contains("integrationtest")) {
+            return Paths.get(resourceLoader.getResource("classpath:$thunderbirdProfile").file.path)
         } else {
             var osName = System.getProperty("os.name").lowercase()
             val thunderbirdProfileParentDir = if (osName.contains("mac")) {
-                Paths.get(System.getProperty("user.home"),"Library","Thunderbird", "Profiles")
+                Paths.get(userHomeDir,"Library","Thunderbird", "Profiles")
             } else if (osName.startsWith("windows")) {
                 Paths.get(System.getenv("appdata"),"Thunderbird", "Profiles")
             } else {
-                Paths.get(System.getProperty("user.home"), ".thunderbird")
+                Paths.get(userHomeDir, ".thunderbird")
             }
             thunderbirdProfileParentDir.resolve(thunderbirdProfile)
         }
--- src/test/kotlin/TbTaskSyncApplicationTests.kt
+++ src/test/kotlin/TbTaskSyncApplicationTests.kt
@@ -1,17 +1,32 @@
 package com.github.gridlts.tbtasksync
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+
 
 @SpringBootTest
 @Import(TestConfig::class)
-@ActiveProfiles("test")
-class TbTaskSyncApplicationTests {
+@ActiveProfiles("integrationtest")
+class TbTaskSyncApplicationTests() {
+
+    companion object {
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun overrideProperties(registry: DynamicPropertyRegistry) {
+            registry.add("appdata") { "classpath:/." }
+            registry.add("user.home") { "classpath:/." }
+        }
+    }
 
     @Test
     fun contextLoads() {
         // Your test code here
     }
+
 }
